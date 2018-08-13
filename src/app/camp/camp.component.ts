6c3e4e5068dbc62c3a4c624749724421fd7bc4a9
lpleo <leonardo.piccoli@arcavita.it>
2018-08-13 15:57:45 +0200

    Added camp component

---
+++ src/app/camp/camp.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camp',
+  template: `
+      <p>
+          camp works!
+      </p>
+  `
+})
+export class CampComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
